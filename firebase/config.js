936d776c173410e76b244d2bd5bed2276b4fa550
niknikolaou <nik.nikolaou@gmail.com>
2023-04-02 13:42:49 +0300

    add firestore databse to your project

--- firebase/config.js
+++ firebase/config.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps } from "firebase/app";
+import { initializeApp, getApps, firebase } from "firebase/app";
 
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -13,5 +13,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 let firebase_app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+
 
 export default firebase_app;
--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,9 +1,23 @@
 import ProtectedRoute from "../components/ProtectedRoute";
 import { useAuth } from "@/context/AuthContext";
+import {firebase_app} from "../firebase/config";
+import  addData  from "../firebase/firestore/addData";
 
 
 const DashboardPage = () => {
   const { user } = useAuth();
+
+  const addWallet = async () => {
+    const data = {
+      name: 'John snow',
+      house: 'Stark'
+    }
+    const { result, error } = await addData('wallets', user.uid, data)
+
+    if (error) {
+      return console.log(error)
+    }
+  };
 
     return (
         <ProtectedRoute>
@@ -13,6 +27,7 @@ const DashboardPage = () => {
           <h1>Hello {user.email}</h1>
           
           <h2 className="text-2xl font-semibold">You are logged in, this is dashboard page!</h2>
+          <button onClick={addWallet}>Add Data</button>
         </div>
       </div>
     
